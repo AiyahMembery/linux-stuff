d05880fefb689b2b1aac3c6e6f2b327922b0458d
Aiyah M <56224427+AiyahMembery@users.noreply.github.com>
2021-03-12 21:32:10 +1100

    refiling and cleaning the main bash

--- software apt installer.sh
+++ software apt installer.sh
@@ -1,21 +1,41 @@
-#welcome aiyah this is a basic setup script for ubuntu
+printf "welcome aiyah this is a basic setup script for ubuntu\n"
 sudo apt update;
 sudo apt upgrade;
 
+#flatpack repo
 sudo add-apt-repository ppa:alexlarsson/flatpak
-auso apt update
-sudo apt purge gnome-2048 aisleriot atomix gnome-chess five-or-more hitori iagno gnome-klotski lightsoff gnome-mahjongg gnome-mines gnome-nibbles quadrapassel four-in-a-row gnome-robots gnome-sudoku swell-foop tali gnome-taquin gnome-tetravex libre-office
-sudo apt install gnome-software gnome-tweaks flatpack gnome-software-plugin-flatpak
+sudo apt update
+
+#removeing gnome bloatware
+sudo apt purge gnome-2048 aisleriot atomix gnome-chess five-or-more hitori iagno gnome-klotski lightsoff gnome-mahjongg gnome-mines gnome-nibbles quadrapassel four-in-a-row gnome-robots gnome-sudoku swell-foop tali gnome-taquin gnome-tetravex libre-office snapd
+sudo apt autoremove
+
+#installing basic packages for use with gnome as well as flatpack
+sudo apt install gnome-software gnome-tweaks flatpack gnome-software-plugin-flatpak apt-transport-https dirmngr
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-cp "rec/.bash_aliases" "${HOME}"
-cp -a "gnome/.icons" "${HOME}"
-cp -a "gnome/.themes" "${HOME}"
+#installing Visual Studio for debian
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/debian vs-buster main"
+sudo tee /etc/apt/sources.list.d/mono-official-vs.list
+sudo apt update
 
+sudo apt install monodevelop
+
+#setting up home directory
+cp "home/.bash_aliases" "${HOME}"
+cp -a "home/.icons" "${HOME}"
+cp -a "home/.themes" "${HOME}"
+
+#installing propriatary software
 sudo dpkg -i "sft/google-chrome-stable_current_amd64.deb"
 sudo dpkg -i "sft/minecraft.deb"
 sudo dpkg -i "sft/code_1.54.1-1614898113_amd64.deb"
 sudo apt update
 sudo apt upgrade
 
+#final clean up
 sudo apt autoremove
+
+printf "\n"
+printf "all done!\n"
