253a7f497e0fd69689c8fe0aed9e8a77d4a96c53
Aiyah M <56224427+AiyahMembery@users.noreply.github.com>
2021-03-12 21:02:44 +1100

    setup

--- software apt installer.sh
+++ software apt installer.sh
@@ -8,4 +8,13 @@ alias install='sudo apt install';
 alias remove='sudo apt remove';
 alias purge='sudo apt purge';
 alias clean='sudo apt autoremove';
+sudo apt purge gnome-2048 aisleriot atomix gnome-chess five-or-more hitori iagno gnome-klotski lightsoff gnome-mahjongg gnome-mines gnome-nibbles quadrapassel four-in-a-row gnome-robots gnome-sudoku swell-foop tali gnome-taquin gnome-tetravex
 
+
+cp "rec/.bash_aliases" "${HOME}"
+cp -a "gnome/.icons" "${HOME}"
+cp -a "gnome/.themes" "${HOME}"
+
+sudo dpkg -i "sft/google-chrome-stable_current_amd64.deb"
+sudo dpkg -i "sft/minecraft.deb"
+sudo dpkg -i "sft/code_1.54.1-1614898113_amd64.deb"
