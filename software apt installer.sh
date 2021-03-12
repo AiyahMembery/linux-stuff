e5ad879771c03fbeb83f3b8f32b3150e619fcec9
Aiyah M <56224427+AiyahMembery@users.noreply.github.com>
2021-03-12 21:19:11 +1100

    Update software apt installer.sh

--- software apt installer.sh
+++ software apt installer.sh
@@ -1,15 +1,12 @@
-#this is a simple script to set up all my basic app needs for gnu/linux most recent version this works on is UBUNUTU 20.10
-
+#welcome aiyah this is a basic setup script for ubuntu
 sudo apt update;
 sudo apt upgrade;
 
-alias fix='sudo apt satisfy';
-alias install='sudo apt install';
-alias remove='sudo apt remove';
-alias purge='sudo apt purge';
-alias clean='sudo apt autoremove';
-sudo apt purge gnome-2048 aisleriot atomix gnome-chess five-or-more hitori iagno gnome-klotski lightsoff gnome-mahjongg gnome-mines gnome-nibbles quadrapassel four-in-a-row gnome-robots gnome-sudoku swell-foop tali gnome-taquin gnome-tetravex
-
+sudo add-apt-repository ppa:alexlarsson/flatpak
+auso apt update
+sudo apt purge gnome-2048 aisleriot atomix gnome-chess five-or-more hitori iagno gnome-klotski lightsoff gnome-mahjongg gnome-mines gnome-nibbles quadrapassel four-in-a-row gnome-robots gnome-sudoku swell-foop tali gnome-taquin gnome-tetravex libre-office
+sudo apt install gnome-software gnome-tweaks flatpack gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 cp "rec/.bash_aliases" "${HOME}"
 cp -a "gnome/.icons" "${HOME}"
@@ -18,3 +15,7 @@ cp -a "gnome/.themes" "${HOME}"
 sudo dpkg -i "sft/google-chrome-stable_current_amd64.deb"
 sudo dpkg -i "sft/minecraft.deb"
 sudo dpkg -i "sft/code_1.54.1-1614898113_amd64.deb"
+sudo apt update
+sudo apt upgrade
+
+sudo apt autoremove
