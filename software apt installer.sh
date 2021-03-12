ceab03997b67bef7d67db5da40e9743b3aae3275
Aiyah M <56224427+AiyahMembery@users.noreply.github.com>
2021-03-12 20:08:16 +1100

    Create software apt installer.sh

---
+++ software apt installer.sh
@@ -0,0 +1,11 @@
+#this is a simple script to set up all my basic app needs for gnu/linux most recent version this works on is UBUNUTU 20.10
+
+sudo apt update;
+sudo apt upgrade;
+
+alias fix='sudo apt satisfy';
+alias install='sudo apt install';
+alias remove='sudo apt remove';
+alias purge='sudo apt purge';
+alias clean='sudo apt autoremove';
+
