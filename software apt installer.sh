040cff1f98405598a20efea497f92450cfd29dfc
Aiyah M <56224427+AiyahMembery@users.noreply.github.com>
2021-07-05 00:38:23 +1000

    edits

--- software apt installer.sh
+++ software apt installer.sh
@@ -3,11 +3,12 @@ sudo apt update;
 sudo apt upgrade;
 
 #removeing gnome bloatware
-sudo apt purge gnome-2048 aisleriot atomix gnome-chess five-or-more hitori iagno gnome-klotski lightsoff gnome-mahjongg gnome-mines gnome-nibbles quadrapassel four-in-a-row gnome-robots gnome-sudoku swell-foop tali gnome-taquin gnome-tetravex libre-office snapd
-sudo apt autoremove
+sudo apt purge -y gnome-2048 aisleriot atomix gnome-chess five-or-more hitori iagno gnome-klotski lightsoff gnome-mahjongg gnome-mines gnome-nibbles quadrapassel four-in-a-row gnome-robots gnome-sudoku swell-foop tali gnome-taquin gnome-tetravex snap-store
+sudo apt purge -y libreoffice*
+sudo apt autoremove -y
 
 #installing basic packages for use with gnome as well as flatpack
-sudo apt install gnome-software gnome-tweaks  gnome-software-plugin-flatpak apt-transport-https dirmngr git
+sudo apt install -y gnome-software gnome-tweaks flatpak gnome-software-plugin-flatpak apt-transport-https dirmngr git
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo flatpack
 
 
@@ -21,6 +22,8 @@ sudo dpkg -i "sft/google-chrome-stable_current_amd64.deb"
 sudo dpkg -i "sft/minecraft.deb"
 sudo dpkg -i "sft/code_1.54.1-1614898113_amd64.deb"
 git clone https://github.com/vinceliuice/Layan-gtk-theme.git
+cd layan-gtk-theme
+sudo ./install.sh
 sudo apt update
 sudo apt upgrade
 
